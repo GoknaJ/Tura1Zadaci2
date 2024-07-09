69fa42e231aba89a7291c9422d70e18b93886eb3
Janko Gakovski <gakovski.janko@outlook.com>
2024-07-09 20:55:13 +0200

    Novo

---
+++ Osoba.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tura1Zadaci2
+{
+    public class Osoba : IEquatable<Osoba>, IComparable<Osoba>
+    {
+        public int Dob {  get; set; }
+
+        public int CompareTo(Osoba other)
+        {
+            if (other == null) return 1;
+            return Dob.CompareTo(other.Dob);
+        }
+
+        public bool Equals(Osoba other)
+        {
+            if (other == null) return false;
+            return Dob == other.Dob;
+        }
+
+        public static bool operator ==(Osoba osoba1, Osoba osoba2)
+        {
+            if (((object)osoba1) == null || ((object)osoba2) == null)
+                return Equals(osoba1, osoba2);
+
+            return osoba1.Equals(osoba2);
+        }
+
+        public static bool operator !=(Osoba osoba1, Osoba osoba2)
+        {
+            if (((object)osoba1) == null || ((object)osoba2) == null)
+                return !Equals(osoba1, osoba2);
+
+            return !(osoba1 == osoba2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading;
+using Tura1Zadaci2;
 
 namespace ANapredno
 {
@@ -8,7 +11,121 @@ namespace ANapredno
 
         static void Main(string[] args)
         {
-            
+            //Zadatak1();
+
+            //Zadatak2();
+
+            //Zadatak3();
+
+            //Zadatak4();
+
+            Dictionary<string, int> rjecnik = new Dictionary<string, int>();
+
+            rjecnik["Pero"] = 3;
+            rjecnik["Iva"] = 5;
+            rjecnik["Marko"] = 4;
+            rjecnik["Ivan"] = 1;
+            rjecnik["Maja"] = 2;
+
+            foreach (var osoba in rjecnik)
+            {
+                Console.WriteLine($"Ključ: {osoba.Key}, Vrijednost: {osoba.Value}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Zadatak4()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("\n");
+            builder.Append("Dobra večer! ");
+            builder.AppendFormat("Ovo je {0} primer {1} StringBuilder-a.", "jednostavan", "klase");
+            builder.AppendLine();
+            builder.AppendLine("Ovo je novi redak.");
+            builder.Append("Ovo je još jedan tekst koji se dodaje na kraj.");
+            builder.Insert(0, " Poštovani, \n");
+            builder.Replace("primer", "primjer");
+            builder.AppendLine("\n");
+            builder.Append("\t\t\t\tOvo je potpis.");
+
+            Console.WriteLine(builder.ToString());
+
+            Console.ReadKey();
+        }
+
+        private static void Zadatak3()
+        {
+            //Queue<T>
+            Queue<string> red = new Queue<string>();
+
+            red.Enqueue("Jabuka");
+            red.Enqueue("Kruška");
+            red.Enqueue("Šljiva");
+
+            Console.WriteLine("Jabuka, Kruška, Šljiva");
+
+            string prviElement = red.Dequeue();
+            Console.WriteLine($"Prvi red: {prviElement}");
+
+            string sljedećiElement = red.Peek();
+            Console.WriteLine($"Sljedeći red: {sljedećiElement}");
+
+            //Stack<T>
+            Stack<string> stog = new Stack<string>();
+
+            stog.Push("1. mjesto");
+            stog.Push("2. mjesto");
+            stog.Push("3. mjesto");
+
+            Console.WriteLine("\n1, 2, 3. mjesto");
+
+            string zadnjiElement = stog.Pop();
+            Console.WriteLine($"Zadnji stog: {zadnjiElement}");
+
+            string prethodniElement = stog.Peek();
+            Console.WriteLine($"Prethodni stog: {prethodniElement}");
+
+            Console.ReadKey();
+        }
+
+        private static void Zadatak2()
+        {
+            Osoba[] osobe = new Osoba[]
+                        {
+                new Osoba { Dob = 30 },
+                new Osoba { Dob = 25 },
+                new Osoba { Dob = 40 }
+                        };
+
+            Array.Sort(osobe);
+
+            foreach (var osoba in osobe)
+            {
+                Console.WriteLine($"Dob: {osoba.Dob}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Zadatak1()
+        {
+            var osoba1 = new Osoba
+            {
+                Dob = 20
+            };
+
+            var osoba2 = new Osoba
+            {
+                Dob = 20
+            };
+
+            Console.WriteLine($"Jesu li osobe jednake(Equals)? {osoba1.Equals(osoba2)}");
+
+            Console.WriteLine($"Jesu li osobe jednake(==)? {osoba1 == osoba2}");
+
+            Console.ReadKey();
         }
     }
 }
